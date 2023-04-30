2ff2626c8645f987d3ea9d5c25b8997a89cf02b5
diriczizsolt <z.diriczi@yahoo.com>
2023-04-30 15:32:24 +0300

    HR-10 HR-11 Added GUI for AddProperty form

--- app/src/main/java/com/example/home_rental/LoginActivity.kt
+++ app/src/main/java/com/example/home_rental/LoginActivity.kt
@@ -98,7 +98,7 @@ class LoginActivity : AppCompatActivity() {
                             }
                         }
                         else {
-                        Intent(this, MainActivity2::class.java).also {
+                        Intent(this, OwnerMainActivity::class.java).also {
                             it.flags =
                                 Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                             startActivity(it)
--- app/src/main/java/com/example/home_rental/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/home_rental/ui/gallery/GalleryFragment.kt
@@ -1,37 +1,76 @@
 package com.example.home_rental.ui.gallery
 
+
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.home_rental.R
 import com.example.home_rental.databinding.FragmentGalleryBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.core.Context
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class GalleryFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
+    private val tip = arrayOf<String>("Garsoniera", "Apratament", "Casa", "Vila", "Birou", "Spatiu Comercial", "Spatiu industrial")
+    private val judete = arrayOf("Alba", "Arad", "Arges", "Bacau", "Bihor", "Bistrita-Nasaud", "Botosani", "Braila", "Brasov", "Bucuresti", "Buzau", "Calarasi", "Caras-Severin", "Cluj", "Constanta", "Covasna", "Dambovita", "Dolj", "Galati", "Giurgiu", "Gorj", "Harghita", "Hunedoara", "Ialomita", "Iasi", "Ilfov", "Maramures", "Mehedinti", "Mures", "Neamt", "Olt", "Prahova", "Salaj", "Satu Mare", "Sibiu", "Suceava", "Teleorman", "Timis", "Tulcea", "Valcea", "Vaslui", "Vrancea")
+    private lateinit var autoCompleteTextView: AutoCompleteTextView
+    private lateinit var adapterItems: ArrayAdapter<String>
+    private lateinit var storageRef: StorageReference
+    private lateinit var auth: FirebaseAuth
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        auth = FirebaseAuth.getInstance()
+
+        autoCompleteTextView = binding.actType
+        adapterItems = ArrayAdapter<String>(requireActivity(), R.layout.list_item, tip)
+
+        autoCompleteTextView.setAdapter(adapterItems)
+        autoCompleteTextView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+            Toast.makeText(requireContext(), "Ai selectat: $selectedItem", Toast.LENGTH_SHORT)
+                .show()
         }
+
+        autoCompleteTextView = binding.actJudet
+        adapterItems = ArrayAdapter<String>(requireActivity(), R.layout.list_item, judete)
+
+        autoCompleteTextView.setAdapter(adapterItems)
+        autoCompleteTextView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+            Toast.makeText(requireContext(), "Ai selectat: $selectedItem", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        storageRef = Firebase.storage.reference
+
+        binding.btnAddImage.setOnClickListener {
+            val galleryIntent = Intent(Intent.ACTION_PICK)
+            galleryIntent.type = "image/*"
+            imagePickerActivityResult.launch(galleryIntent)
+        }
+
+
         return root
     }
 
@@ -39,4 +78,42 @@ class GalleryFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private var imagePickerActivityResult: ActivityResultLauncher<Intent> =
+        registerForActivityResult( ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result != null) {
+                val imageUri: Uri? = result.data?.data
+                val sd = getFileName(requireContext(), imageUri!!)
+                val uid = auth.currentUser?.uid
+                val uploadTask = storageRef.child("$uid/$sd").putFile(imageUri)
+                /*uploadTask.addOnSuccessListener {
+                    // using glide library to display the image
+                    storageRef.child("upload/$sd").downloadUrl.addOnSuccessListener {
+                        Glide.with(this)
+                            .load(it)
+                            .into(binding.ivImage)
+
+                        Log.e("Firebase", "download passed")
+                    }.addOnFailureListener {
+                        Log.e("Firebase", "Failed in downloading")
+                    }
+                }.addOnFailureListener {
+                    Log.e("Firebase", "Image Upload fail")
+                }*/
+            }
+        }
+
+    private fun getFileName(context: android.content.Context, uri: Uri): String? {
+        if (uri.scheme == "content") {
+            val cursor = context.contentResolver.query(uri, null, null, null, null)
+            cursor.use {
+                if (cursor != null && cursor.moveToFirst()) {
+                    return cursor.getString(cursor.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME))
+                }
+            }
+        }
+        return uri.path?.substring(uri.path!!.lastIndexOf("/") + 1)
+    }
+
+
 }
