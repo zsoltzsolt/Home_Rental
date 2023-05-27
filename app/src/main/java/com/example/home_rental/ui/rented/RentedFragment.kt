1c010998ff5d30240aec992b532066879864a057
diriczizsolt <z.diriczi@yahoo.com>
2023-05-27 16:41:49 +0300

    HR-24 HR-33 Retrieved infromation from database

--- app/src/main/java/com/example/home_rental/ui/rented/RentedFragment.kt
+++ app/src/main/java/com/example/home_rental/ui/rented/RentedFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.home_rental.Properties
 import com.example.home_rental.PropertyAdapter
 import com.example.home_rental.R
 import com.example.home_rental.databinding.FragmentHomeBinding
-import com.example.home_rental.databinding.FragmentSlideshowBinding
 import com.example.home_rental.ui.details.DetailsFragment
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
